18fbba92fb6c063e8e6b14d1bc8afd02d2583c4c
Jamie Yeung <jamiethy@gmail.com>
2025-11-11 09:58:24 +0000

    merge similar classes support

--- preprocessing/extract_landmarks.py
+++ preprocessing/extract_landmarks.py
@@ -13,7 +13,8 @@ csv_path = "../model_data/training_data.csv"
 # for reference. 'N/A' and 'no_gesture' should not be encountered.
 class_id_map = {"fist": 0, "like": 1, "dislike": 2, "one": 3, "middle_finger": 4, "little_finger": 5, "thumb_index": 6,
                 "call": 7, "peace": 8, "two_up": 9, "rock": 10, "three_gun": 11, "three2": 12, "three": 13, "three3": 14,
-                "ok": 15, "four": 16, "palm": 17, "grip": 18, "stop": 19, "grabbing": 20, "N/A": 21, "no_gesture": 22}
+                "ok": 15, "four": 16, "palm": 17, "grip": 18, "stop": 19, "grabbing": 20, "N/A": 21, "no_gesture": 22,
+                "peace_inverted": 8, "two_up_inverted": 9, "stop_inverted": 19}
 
 # center + noramlise landmark coordinates
 def process(landmarks):
@@ -27,45 +28,49 @@ def process(landmarks):
     flattened = [coord for pair in normalised for coord in pair]
     return flattened
 
-with open(json_path, "r") as f:
-    data = json.load(f)
+def write_json(json_path, csv_path, write=False):
+    with open(json_path, "r") as f:
+        data = json.load(f)
 
-# write to destination csv
-with open(csv_path, "a", newline="") as f:
-    writer = csv.writer(f)
-    
-    for entry_id, entry in data.items():
-        if not entry.get("hand_landmarks"):
-            continue
+    method = "w" if write else "a"
+
+    # write to destination csv
+    with open(csv_path, method, newline="") as f:
+        writer = csv.writer(f)
         
-        # extract gesture label
-        index = 0
-        if entry.get("labels"):
-            if entry["labels"][0] != "no_gesture":
-                label = entry["labels"][0]
+        for _, entry in data.items():
+            if not entry.get("hand_landmarks"):
+                continue
+            
+            # extract gesture label
+            index = 0
+            if entry.get("labels"):
+                if entry["labels"][0] != "no_gesture":
+                    label = entry["labels"][0]
+                else:
+                    label = entry["labels"][1] if len(entry["labels"]) > 1 else "N/A"
+                    index = 1
             else:
-                label = entry["labels"][1] if len(entry["labels"]) > 1 else "N/A"
-                index = 1
-        else:
-            label = "N/A"
+                label = "N/A"
 
-        id = class_id_map[label]
+            id = class_id_map[label]
 
-        # cannot be classified
-        if id == 21 or id == 22:
-            continue
+            # cannot be classified
+            if id == 21 or id == 22:
+                continue
 
-        # only coordinates for the hand performing the gesture is needed. 
-        # it is possible that there are no landmarks available for a specific entry.
-        landmarks = entry["hand_landmarks"][index] if entry["hand_landmarks"][index] else []
+            # only coordinates for the hand performing the gesture is needed. 
+            # it is possible that there are no landmarks available for a specific entry.
+            landmarks = entry["hand_landmarks"][index] if entry["hand_landmarks"][index] else []
 
-        if not landmarks:
-            continue
+            if not landmarks:
+                continue
 
-        # center by the wrist, scale by the max norm and flatten the 21 pairs of coordinates
-        processed_landmarks = process(landmarks)
-        
-        # [class_id, x1, y1, ..., x21, y21]
-        writer.writerow([id] + processed_landmarks)
+            # center by the wrist, scale by the max norm and flatten the 21 pairs of coordinates
+            processed_landmarks = process(landmarks)
+            
+            # [class_id, x1, y1, ..., x21, y21]
+            writer.writerow([id] + processed_landmarks)
 
+write_json(json_path, csv_path)
 print("done")
--- preprocessing/merge_data.py
+++ preprocessing/merge_data.py
@@ -1,4 +1,49 @@
 # Used to merge inverted gestures into one dataset, before adding them to the csv file.
 # Inverted: hand's palm faces away from camera.
 
-print("")
+import pandas as pd
+import csv
+from extract_landmarks import write_json
+
+# total number of samples after merging.
+N = 24000
+half = N // 2
+
+# seed used for sampling
+SEED = 100
+
+# path1 for non-inverted class, path2 for inverted class.
+path1 = "../annotations/train/peace.json"
+path2 = "../annotations/train/peace_inverted.json"
+
+# paths for temporary storage of both classes
+temp_path1 = "./preprocessing/temp/class1.csv"
+temp_path2 = "./preprocessing/temp/class2.csv"
+
+# the merged samples will be placed into this dataset.
+csv_path = "../model_data/training_data.csv"
+
+# for reference. 'N/A' and 'no_gesture' should not be encountered.
+class_id_map = {"fist": 0, "like": 1, "dislike": 2, "one": 3, "middle_finger": 4, "little_finger": 5, "thumb_index": 6,
+                "call": 7, "peace": 8, "two_up": 9, "rock": 10, "three_gun": 11, "three2": 12, "three": 13, "three3": 14,
+                "ok": 15, "four": 16, "palm": 17, "grip": 18, "stop": 19, "grabbing": 20, "N/A": 21, "no_gesture": 22,
+                "peace_inverted": 8, "two_up_inverted": 9, "stop_inverted": 19}
+
+# write both classes into separate files (overwrite pre-existing file)
+write_json(path1, temp_path1, True)
+write_json(path2, temp_path2, True)
+
+# load and merge N//2 samples from each class
+normal = pd.read_csv(temp_path1)
+inverted = pd.read_csv(temp_path2)
+
+normal_samples = normal.sample(n=half, random_state=SEED)
+inverted_samples = inverted.sample(n=half, random_state=SEED)
+
+merged_samples = pd.concat([normal_samples, inverted_samples])
+merged_samples = merged_samples.sample(frac=1, random_state=SEED).reset_index(drop=True)  # shuffle merged dataset
+
+# place merged samples in the target dataset file
+merged_samples.to_csv(csv_path, mode="a", header=False, index=False)
+
+print("done")
