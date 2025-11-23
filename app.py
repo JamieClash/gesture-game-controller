2766ca4e3294217ac7ddb28ea0ee91df4e2505ea
Jamie Yeung <jamiethy@gmail.com>
2025-11-23 19:30:14 +0000

    Added no_gesture class data

--- app.py
+++ app.py
@@ -243,22 +243,25 @@ def pre_process_landmark(landmark_list):
 
     # Convert to relative coordinates
     base_x, base_y = 0, 0
+    norms = []
+
     for index, landmark_point in enumerate(temp_landmark_list):
         if index == 0:
             base_x, base_y = landmark_point[0], landmark_point[1]
 
         temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
         temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+        norms.append(np.linalg.norm([temp_landmark_list[index][0], temp_landmark_list[index][1]]))
 
     # Convert to a one-dimensional list
     temp_landmark_list = list(
         itertools.chain.from_iterable(temp_landmark_list))
 
     # Normalization
-    max_value = max(list(map(abs, temp_landmark_list)))
+    max_norm = max(norms) if norms else 1e-6
 
     def normalize_(n):
-        return n / max_value
+        return n / max_norm
 
     temp_landmark_list = list(map(normalize_, temp_landmark_list))
 
@@ -292,7 +295,7 @@ def logging_csv(number, mode, landmark_list, point_history_list):
     if mode == 0:
         pass
     if mode == 1 and (0 <= number <= 9):
-        csv_path = '../model_data/training_data.csv'
+        csv_path = 'model/keypoint_classifier/keypoint.csv'
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([number, *landmark_list])
---
+++ preprocessing/split_sets.py
@@ -0,0 +1,24 @@
+# Split a given csv file's samples into training, test and validation sets
+# and append them into destination dataset files.
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# seed used for sampling
+SEED = 100
+
+src_path = "./model/keypoint_classifier/keypoint.csv"
+train_path = "../model_data/training_data.csv"
+test_path = "../model_data/testing_data.csv"
+val_path = "../model_data/validation_data.csv"
+
+df = pd.read_csv(src_path)
+df.iloc[:, 0] = 22  # replace labels with no_gesture's numeric label
+
+# sample into separate sets, and store them in the correponding paths.
+train_df, temp_df = train_test_split(df, test_size=0.25, random_state=SEED)  # 0.75 - training set
+test_df, val_df = train_test_split(temp_df, test_size=0.5, random_state=SEED)  # 0.125 - testing, 0.125 - validation
+
+train_df.to_csv(train_path, mode="a", header=False, index=False)
+test_df.to_csv(test_path, mode="a", header=False, index=False)
+val_df.to_csv(val_path, mode="a", header=False, index=False)
